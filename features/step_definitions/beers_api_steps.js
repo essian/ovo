d5b4a6dbfa0fa32263c496153e9695d7ad26d50e
Jessica Dennis <jessicampdennis@hotmail.com>
2022-09-05 21:27:11 +0100

    refactor

--- features/step_definitions/beers_api_steps.js
+++ features/step_definitions/beers_api_steps.js
@@ -8,25 +8,25 @@ let maltList
 let firstMalt
 
 Given('I call the punk api with beer id {int}', async function (id) {
-      response = await axios.get(`https://api.punkapi.com/v2/beers/${id}`)
-      data = response.data
+    response = await axios.get(`https://api.punkapi.com/v2/beers/${id}`)
+    data = response.data
     });
 
 // This step is redundant. The Given step will fail for a non-200 response code unless it's caught
-Then('I expect a {int} status response', function (int) {
-        assert.equal(response.status, 200)
+Then('I expect a {int} status response', function (statusCode) {
+    assert.strictEqual(response.status, statusCode)
     });
 
 Then('The malt is “Extra Pale”', function () {
     maltList = data[0].ingredients.malt
-    assert.equal(maltList.length, 1)
     firstMalt = maltList[0]
-    assert.equal(firstMalt.name, "Extra Pale")
+    assert.strictEqual(maltList.length, 1)
+    assert.strictEqual(firstMalt.name, "Extra Pale")
     });
 
 Then('The malt value is {float} and the unit is “kilograms”', function (expectedMaltValue) {
     const maltValue = firstMalt.amount.value
     const maltUnit = firstMalt.amount.unit
-    assert.equal(maltValue, expectedMaltValue)
-    assert.equal(maltUnit, "kilograms")
+    assert.strictEqual(maltValue, expectedMaltValue)
+    assert.strictEqual(maltUnit, "kilograms")
     });
